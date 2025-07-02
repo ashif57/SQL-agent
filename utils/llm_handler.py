8f2f23e19fe6460e8d74e83d984f18625b4d6c0c
Ashif navheed A <ashifnavheed@gmail.com>
2025-07-02 15:50:10 +0530

    streamlit llm_handler.py

--- utils/llm_handler.py
+++ utils/llm_handler.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("API_KEY"),
+    api_key=st.secrets["API_KEY"],
 )
 
 def call_llm(prompt, model="deepseek/deepseek-r1-0528-qwen3-8b:free", temperature=0.3):
