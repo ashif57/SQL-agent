4744f0fbd6fe08376166d49563975e2ef2922c59
ashif57 <ashifnavheed@gmail.com>
2025-07-02 17:08:27 +0530

    error
    .

--- utils/parser_utils.py
+++
@@ -1,7 +0,0 @@
-import sqlparse
-
-def format_sql(raw_sql: str) -> str:
-    """
-    Formats SQL with proper indentation and uppercase keywords.
-    """
-    return sqlparse.format(raw_sql, reindent=True, keyword_case='upper')
